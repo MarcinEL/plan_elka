a15b919d67862a179ec1083d3c8df0b6fd3bf0c4
marcinel <marcinlewandowski.poczta@gmail.com>
2026-04-27 17:55:02 +0200

    Wsparcie dla telefonów i tabletów

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { polyfill } from "mobile-drag-drop";
+import "mobile-drag-drop/default.css";
+
+// Aplikuj polyfill dla urządzeń dotykowych
+polyfill({
+  dragImageTranslateOverride: "none",
+  holdToDrag: 300 // Wymaga przytrzymania palca przez 300ms, co ułatwia normalne przewijanie strony
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
